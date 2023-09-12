47f276b4db69e345e98779d6234c41cb51bf627e
Stefano Martins <xstefanox@gmail.com>
2023-09-12 09:11:28 -0300

    Criando aulas e diretórios sobre vários assuntos

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
 
-links = {}
+links = {
+    'assunto-01': {
+        'aula-01': 'link-para-aula.m3u8',
+    },
+    'assunto-02': {}
+}
 
-for k,v in links.items():
-    print(f"Convertendo arquivo {k}")
-    os.system(f"ffmpeg -v quiet -stats -i {v} -c copy -bsf:a aac_adtstoasc {k}.mp4")
+
+for day_k, day_v in links.items():
+    print(f"Criando diretório {day_v}")
+    os.system(f"mkdir {day_k}")
+    for file_name, file_url in day_v.items():
+        print(f"Convertendo o arquivo {file_name}")
+        os.system(f"ffmpeg -v quiet -stats -i {file_url} -c copy -bsf:a aac_adtstoasc {day_k}/{file_name}.mp4")

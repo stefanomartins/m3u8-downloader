e4d213b02426c3174acec59490ba23dd6ea32800
Stefano Martins <xstefanox@gmail.com>
2023-09-14 20:53:42 -0300

    Suporte a subdiretórios

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ links = {
 
 for day_k, day_v in links.items():
     print(f"Criando diretório {day_v}")
-    os.system(f"mkdir {day_k}")
+    os.system(f"mkdir '{day_k}'")
     for file_name, file_url in day_v.items():
         print(f"Convertendo o arquivo {file_name}")
-        os.system(f"ffmpeg -v quiet -stats -i {file_url} -c copy -bsf:a aac_adtstoasc {day_k}/{file_name}.mp4")
+        os.system(f"ffmpeg -v quiet -stats -i {file_url} -c copy -bsf:a aac_adtstoasc '{day_k}/{file_name}.mp4'")

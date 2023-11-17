f7803beb3b1c726872179e576fd1e5a7c1f2c211
Stefano Martins <xstefanox@gmail.com>
2023-11-17 18:17:15 -0300

    Adicionando a funcionalidade de paralelismo e adicionado arquivo de requirements.txt

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-import os
 import ffmpeg
+import os
+import concurrent.futures
 
 links = {"subject-01": {}, "subject-02": {}}
 
 
+def download_file(file_name: str, file_url: str):
+    ffmpeg.input(
+        file_url,
+    ).output(
+        f"{file_name}.mp4", absf="aac_adtstoasc", c="copy"
+    ).run(quiet=True, overwrite_output=True)
+
+
 def main():
-    for day_k, day_v in links.items():
-        if not os.path.exists(day_k):
-            print(f"Criando diretório {day_k}")
-            os.mkdir(day_k)
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = []
+        for day_k, day_v in links.items():
+            if not os.path.exists(day_k):
+                print(f"Criando diretório {day_k}")
+                os.mkdir(day_k)
 
-        for file_name, file_url in day_v.items():
-            print(f"Convertendo o arquivo {file_name}")
-            ffmpeg.input(
-                file_url,
-            ).output(
-                f"{day_k}/{file_name}.mp4", absf="aac_adtstoasc", c="copy"
-            ).run(quiet=True, overwrite_output=True)
+            for file_name, file_url in day_v.items():
+                print(f"Convertendo o arquivo {file_name}")
 
-            # NOTE: Old syntax. Commented for historic purposes.
-            # os.system(f"ffmpeg -v quiet -stats -i {file_url} -c copy -bsf:a aac_adtstoasc '{day_k}/{file_name}.mp4'")
+                results.append(
+                    executor.submit(download_file, f"{day_k}/{file_name}", file_url)
+                )
+
+                # NOTE: Old syntax. Commented for historic purposes.
+                # os.system(f"ffmpeg -v quiet -stats -i {file_url} -c copy -bsf:a aac_adtstoasc '{day_k}/{file_name}.mp4'")
 
 
 if __name__ == "__main__":
